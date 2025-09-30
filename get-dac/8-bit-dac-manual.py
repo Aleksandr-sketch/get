c783f5922fcaaca939d4151183b42861b9600073
Aleksandr-sketch <aleksmakklaud4695@gmail.com>
2025-09-30 11:04:30 +0300

    8-битный R2R ЦАП в виде скрипта

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(dac_bits, GPIO.OUT)
-dynamic_range = 3.3
+dynamic_range = 3.183
 
 def voltage_to_number(voltage):
     global dynamic_range
